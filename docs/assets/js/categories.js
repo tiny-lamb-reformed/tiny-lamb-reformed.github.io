b3160195d873cc8fbabb1236b2484260913a3076
Ken Hung <eiekenhung@gmail.com>
2022-03-20 16:40:47 +0800

    filter posts

--- docs/assets/js/categories.js
+++ docs/assets/js/categories.js
@@ -1,10 +1,24 @@
 function hideCategoriesExcept(category) {
-  list = document.querySelectorAll('.archive section');
+  var list = document.querySelectorAll('.archive section');
   for (i = 0; i < list.length; i++) {
     var element = list[i];
     element.style.display = (element.id == category ? 'block' : 'none');
   }
 }
+
+function showOnlyMatch() {
+  var keyword_list = $('#search')[0].value.split(' ');
+  var filter = '';
+  for (var i = 0; i < keyword_list.length; i++) {
+    var keyword = keyword_list[i];
+    filter += 'a:contains("' + keyword + '")';
+  }
+  $('div.list__item').show();
+  if (filter) {
+    $('div.list__item:not(:has(' + filter + '))').hide();
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   var current = decodeURI(window.location.hash).substring(1) || '成聖之路';
   hideCategoriesExcept(current);
