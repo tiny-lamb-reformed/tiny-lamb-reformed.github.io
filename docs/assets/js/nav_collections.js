6bfa7d41f450e0c9de6dfaf914d8fc2346b10af2
Ken Hung <eiekenhung@gmail.com>
2022-03-21 14:59:49 +0800

    test hash nav

---
+++ docs/assets/js/nav_collections.js
@@ -0,0 +1,11 @@
+var nav_collections = {
+  '十一、奉獻、濟貧、教會金錢管理': {
+    title: '十一、奉獻、濟貧、教會金錢管理',
+    url: '/abc',
+    children: [
+      { title: 'child title 1', url: '/child/abc' },
+      { title: 'child title 2', url: '/child/abc' },
+      { title: 'child title 2', url: '/child/abc' }
+    ]
+  }
+};
