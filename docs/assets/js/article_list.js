692b5a8e2f8212a1b009c7634c8cdb75ac377f11
Ken Hung <eiekenhung@gmail.com>
2022-10-14 07:00:34 +0800

    test list layout

--- docs/assets/js/article_list.js
+++ docs/assets/js/article_list.js
@@ -13,20 +13,41 @@ var simpleList = new List('main', {
   valueNames: valueNames
 });
 
+window.onhashchange = refreshList;
 
-window.onhashchange = filteritems;
+var windowWidth = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+if (windowWidth < 600) {
+  hideDetails();
+}
+else {
+  showDetails();
+}
 
-function filteritems() {
+function refreshList() {
   detailedList.filter(categorySelected);
   simpleList.filter(categorySelected);
+  var currentSelected = decodeURI(location.hash).substring(1).replace(/\-/g, '、');
+  updateBreadCrumb(currentSelected);
 }
 
 function categorySelected(item) {
-  if (location.hash === '') {
-    return true;
-  }
-  else {
-    var category = item.values()['category'].replace(/、/g, '-');
-    return category === decodeURI(location.hash).substring(1);
-  }
+  var category = item.values()['category'].replace(/、/g, '-');
+  var currentSelected = decodeURI(location.hash).substring(1);
+  return currentSelected === '' ? true : category === currentSelected;
+}
+
+function showDetails() {
+  document.querySelector('#detailed-list').style.display = '';
+  document.querySelector('#simple-list').style.display = 'none';
+}
+
+function hideDetails() {
+  document.querySelector('#detailed-list').style.display = 'none';
+  document.querySelector('#simple-list').style.display = '';
+}
+
+function updateBreadCrumb(category) {
+  document.querySelector('#selected-catagory').textContent = category !== '' ? ' > ' + category : '';
 }
