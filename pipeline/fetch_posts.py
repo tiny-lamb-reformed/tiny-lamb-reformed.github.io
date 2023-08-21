02bcc778fb3f64bc8bc7c6c6ce3b863653114a7b
Ken Hung <eiekenhung@gmail.com>
2023-08-22 05:27:16 +0800

    sync posts/index

--- pipeline/fetch_posts.py
+++ pipeline/fetch_posts.py
@@ -1,5 +1,7 @@
 import json
+import os
 from math import ceil
+from pathlib import Path
 
 import requests
 
@@ -15,40 +17,47 @@ def get_pixnet_articles(id="", params={}):
     return resp.json()
 
 
+articles = []
 PER_PAGE = 500
 page, total_pages = 1, 1
 while page <= total_pages:
     data = get_pixnet_articles(
         params={"status": 2, "trim_user": 1, "page": page, "per_page": PER_PAGE}
     )
-    articles = data["articles"]
+    articles += data["articles"]
     total_pages = ceil(data["total"] / PER_PAGE)
     print(data["message"], f"Page {page}/{total_pages}")
     page += 1
 
-    # Save the articles to files
-    for article in articles:
-        last_modified = article["last_modified"]
+# Save the articles to files
+for article in articles:
+    last_modified = article["last_modified"]
 
-        # Check if the article has been modified since last download
-        filename = f"posts/{article['id']}.json"
-        try:
-            with open(filename, "r") as f:
-                existing_data = json.load(f)
-            existing_last_modified = existing_data["last_modified"]
-            if last_modified == existing_last_modified:
-                continue
-        except FileNotFoundError:
-            pass
-        except json.decoder.JSONDecodeError:
-            pass
+    # Check if the article has been modified since last download
+    filename = f"posts/{article['id']}.json"
+    try:
+        with open(filename, "r") as f:
+            existing_data = json.load(f)
+        existing_last_modified = existing_data["last_modified"]
+        if last_modified == existing_last_modified:
+            continue
+    except FileNotFoundError:
+        pass
+    except json.decoder.JSONDecodeError:
+        pass
 
-        article_data = get_pixnet_articles(article["id"])
-        assert article_data["message"] == "Show article successfully"
-        assert article_data["error"] == 0
-        # Save the article to a file
-        with open(filename, "w") as f:
-            json.dump(article_data["article"], f)
+    article_data = get_pixnet_articles(article["id"])
+    assert article_data["message"] == "Show article successfully"
+    assert article_data["error"] == 0
+    # Save the article to a file
+    with open(filename, "w") as f:
+        json.dump(article_data["article"], f)
 
-# TODO: delete removed posts
-# TODO: assert count
+pixnet_ids = [article["id"] for article in articles]
+file_ids = [file.stem for file in Path("posts").glob("*.json")]
+
+removed_pixnet_ids = set(pixnet_ids) - set(file_ids)
+for pixnet_id in removed_pixnet_ids:
+    os.remove(f"posts/{pixnet_id}.json")
+
+assert len(pixnet_ids) == len(file_ids)
--- pipeline/index.py
+++ pipeline/index.py
@@ -1,38 +1,67 @@
 import json
 import os
+import subprocess
 from pathlib import Path
+from typing import Literal
 
 import opencc
 from algoliasearch.search_client import SearchClient
 from bs4 import BeautifulSoup
 
-# Initialize Algolia client
-client = SearchClient.create("8QSPV5R7NU", os.environ["ALGOLIA_API_KEY"])
-index = client.init_index("posts-tw")
 
-# t2s.json is the configuration file for Traditional to Simplified conversion
-converter = opencc.OpenCC("t2s")
+def main():
+    # Initialize Algolia client
+    client = SearchClient.create("8QSPV5R7NU", os.environ["ALGOLIA_API_KEY"])
+    index = client.init_index("posts-tw")
 
-posts = []
-for json_file in Path("pipeline/posts").glob("*.json"):
-    with open(json_file) as file:
-        post = json.load(file)
-        if post.get("category") != "複習":
-            posts.append(post)
+    # t2s.json is the configuration file for Traditional to Simplified conversion
+    converter = opencc.OpenCC("t2s")
 
-for post in posts:
-    body_text = BeautifulSoup(post["body"], features="lxml").get_text()[:16000]
-    record = {
-        "objectID": post["id"],
-        "title": post["title"],
-        "title_cn": converter.convert(post["title"]),
-        "content": body_text,
-        "content_cn": converter.convert(body_text),
-    }
+    for post in get_posts("deleted"):
+        index.delete_object(post["id"]).wait()
+        print(f"Deleted {post['id']} in Algolia index.")
 
-    # Update Algolia index with the filtered data
-    index.save_object(record).wait()
-    print(f"Saved {post['id']} in Algolia index.")
+    for post in get_posts("changed"):
+        body_text = BeautifulSoup(post["body"], features="lxml").get_text()[:16000]
+        record = {
+            "objectID": post["id"],
+            "title": post["title"],
+            "title_cn": converter.convert(post["title"]),
+            "content": body_text,
+            "content_cn": converter.convert(body_text),
+        }
+        # Update Algolia index with the filtered data
+        index.save_object(record).wait()
+        print(f"Saved {post['id']} in Algolia index.")
 
-# TODO: delete
-assert len(posts) == client.list_indices()["items"][-1]["entries"]
+    assert len(get_posts()) == client.list_indices()["items"][-1]["entries"]
+
+
+def get_posts(mode: Literal["all", "changed", "deleted"] = "all"):
+    if mode == "all":
+        post_paths = Path("posts").glob("*.json")
+    else:
+        post_paths = get_changed_files(mode == "deleted")
+
+    posts = []
+    for json_file in post_paths:
+        with open(json_file) as file:
+            post = json.load(file)
+            if post.get("category") != "複習":
+                posts.append(post)
+    return posts
+
+
+def get_changed_files(deleted):
+    # Run the `git` command to get the changed files in the staging area
+    cmd = ["git", "ls-files", "posts"] + (
+        ["--deleted"] if deleted else ["--modified", "--others"]
+    )
+    output = subprocess.check_output(cmd, universal_newlines=True)
+
+    # Split the output into a list of file paths
+    return output.strip().split("\n") if output else []
+
+
+if __name__ == "__main__":
+    main()
